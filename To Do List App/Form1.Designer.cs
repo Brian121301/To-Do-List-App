ebdb3b0143bada7c5b467ce050a178d446412dc9
Brian121301 <105826951+Brian121301@users.noreply.github.com>
2025-01-16 23:31:49 -0600

    Finished designing UI

--- To Do List App/Form1.Designer.cs
+++ To Do List App/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace To_Do_List_App
 {
-    partial class Form1
+    partial class ToDoList
     {
         /// <summary>
         /// Required designer variable.
@@ -28,21 +28,153 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.mainTitle = new System.Windows.Forms.Label();
+            this.titleInput = new System.Windows.Forms.TextBox();
+            this.descInput = new System.Windows.Forms.TextBox();
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.descLabel = new System.Windows.Forms.Label();
+            this.newButton = new System.Windows.Forms.Button();
+            this.editButton = new System.Windows.Forms.Button();
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.toDoListView = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.toDoListView)).BeginInit();
             this.SuspendLayout();
             // 
-            // Form1
+            // mainTitle
+            // 
+            this.mainTitle.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.mainTitle.AutoSize = true;
+            this.mainTitle.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.mainTitle.Font = new System.Drawing.Font("Verdana", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mainTitle.Location = new System.Drawing.Point(384, 9);
+            this.mainTitle.Name = "mainTitle";
+            this.mainTitle.Size = new System.Drawing.Size(293, 59);
+            this.mainTitle.TabIndex = 0;
+            this.mainTitle.Text = "To Do List";
+            this.mainTitle.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // titleInput
+            // 
+            this.titleInput.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.titleInput.Location = new System.Drawing.Point(12, 101);
+            this.titleInput.Name = "titleInput";
+            this.titleInput.Size = new System.Drawing.Size(620, 20);
+            this.titleInput.TabIndex = 1;
+            // 
+            // descInput
+            // 
+            this.descInput.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.descInput.Location = new System.Drawing.Point(12, 150);
+            this.descInput.Name = "descInput";
+            this.descInput.Size = new System.Drawing.Size(620, 20);
+            this.descInput.TabIndex = 2;
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.titleLabel.Location = new System.Drawing.Point(13, 82);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(740, 16);
+            this.titleLabel.TabIndex = 3;
+            this.titleLabel.Text = "Title:";
+            // 
+            // descLabel
+            // 
+            this.descLabel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.descLabel.Location = new System.Drawing.Point(12, 128);
+            this.descLabel.Name = "descLabel";
+            this.descLabel.Size = new System.Drawing.Size(741, 19);
+            this.descLabel.TabIndex = 4;
+            this.descLabel.Text = "Description";
+            // 
+            // newButton
+            // 
+            this.newButton.Location = new System.Drawing.Point(8, 188);
+            this.newButton.Name = "newButton";
+            this.newButton.Size = new System.Drawing.Size(169, 40);
+            this.newButton.TabIndex = 5;
+            this.newButton.Text = "New";
+            this.newButton.UseVisualStyleBackColor = true;
+            // 
+            // editButton
+            // 
+            this.editButton.Location = new System.Drawing.Point(183, 188);
+            this.editButton.Name = "editButton";
+            this.editButton.Size = new System.Drawing.Size(169, 40);
+            this.editButton.TabIndex = 6;
+            this.editButton.Text = "Edit";
+            this.editButton.UseVisualStyleBackColor = true;
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.Location = new System.Drawing.Point(358, 188);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(169, 40);
+            this.deleteButton.TabIndex = 7;
+            this.deleteButton.Text = "Delete";
+            this.deleteButton.UseVisualStyleBackColor = true;
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(533, 188);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(176, 40);
+            this.saveButton.TabIndex = 8;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            // 
+            // toDoListView
+            // 
+            this.toDoListView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.toDoListView.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.toDoListView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.toDoListView.Location = new System.Drawing.Point(8, 252);
+            this.toDoListView.Name = "toDoListView";
+            this.toDoListView.Size = new System.Drawing.Size(1042, 342);
+            this.toDoListView.TabIndex = 9;
+            // 
+            // ToDoList
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.Gray;
+            this.ClientSize = new System.Drawing.Size(1062, 606);
+            this.Controls.Add(this.toDoListView);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.deleteButton);
+            this.Controls.Add(this.editButton);
+            this.Controls.Add(this.newButton);
+            this.Controls.Add(this.descLabel);
+            this.Controls.Add(this.titleLabel);
+            this.Controls.Add(this.descInput);
+            this.Controls.Add(this.titleInput);
+            this.Controls.Add(this.mainTitle);
+            this.Name = "ToDoList";
+            this.Text = "To Do List";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.toDoListView)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label mainTitle;
+        private System.Windows.Forms.TextBox titleInput;
+        private System.Windows.Forms.TextBox descInput;
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Label descLabel;
+        private System.Windows.Forms.Button newButton;
+        private System.Windows.Forms.Button editButton;
+        private System.Windows.Forms.Button deleteButton;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.DataGridView toDoListView;
     }
 }
 
